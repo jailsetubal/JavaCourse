c06b1db55f0efc8fb203a29940055c7c57a03db1
jailsetubal <jail.setubal@outlook.com>
2025-09-05 16:01:09 -0300

    Aula 05. Continuação das variáveis

---
+++ src/main/java/Aula05/VariaveisChar.java
@@ -0,0 +1,17 @@
+package Aula05;
+
+public class VariaveisChar {
+    public static void main(String[] args) {
+
+        char o = 111;
+        char i = 105;
+        char interrogacao = 0X3F;
+
+        System.out.println(""+o+i);
+        System.out.println(o+i);
+        System.out.println(""+o+i+interrogacao);
+
+
+
+    }
+}
---
+++ src/main/java/Aula05/VariaveisInteiras.java
@@ -0,0 +1,16 @@
+package Aula05;
+
+public class VariaveisInteiras {
+    public static void main(String[] args) {
+
+        byte idade01 = 25;
+        short idade02 = 25;
+        int idade03 = 25;
+        long idade04 = 25;
+
+        System.out.println("O valor da variável é: " + idade01);
+        System.out.println("O valor da variável é: " + idade02);
+        System.out.println("O valor da variável é: " + idade03);
+        System.out.println("O valor da variável é: " + idade04);
+    }
+}
